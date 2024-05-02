a988d1824f07844d990d3cec8b9f725b7dede886
rupom <mohrajulrupom@gmail.com>
2024-05-02 16:59:48 +0600

    add and remove popular and trending products

--- index.php
+++ index.php
@@ -7,54 +7,54 @@
  */
 add_action( 'init', 'custom_orders_page_callback' );
 function custom_orders_page_callback(){
-    $transient_data = get_transient('last_time');
-    // $execution_time = get_option('last_time');
-    // $execution_time += 24 * 3600;
-    // $current_time = current_time('timestamp');
-    // if ($current_time < $execution_time) {
-    //     return; 
-    // }
-    // delete_transient('last_time');
-    var_dump($transient_data);
-    if($transient_data !== false ){
-        return;
+    $current_time = current_time('timestamp');  // get current time
+    $execution_time = get_option('last_time');  //get option current time 
+    $execution_time += 24 * 3600;
+    if ($current_time < $execution_time) {
+        return; 
     }
-    $category_to_remove = 'popular'; 
+    $category_remove_popular = 'popular'; 
+    $category_remove_trending = 'trending'; 
     $args = array(
         'post_type' => 'product',
-        'posts_per_page' => -1,
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'product_cat',
-                'field' => 'slug',
-                'terms' => 'popular'
-            )
-        )
+        'posts_per_page' => -1
     );
     $products_query = new WP_Query($args);
     if ($products_query->have_posts()) {
         while ($products_query->have_posts()) {
             $products_query->the_post();
             $product_id = get_the_ID();
-            wp_remove_object_terms($product_id, $category_to_remove, 'product_cat');
+            wp_remove_object_terms($product_id, $category_remove_popular, 'product_cat');
+            wp_remove_object_terms($product_id, $category_remove_trending, 'product_cat');
         }
         wp_reset_postdata();
     }
-    // get last 10 days order products
-    $days_ago = date('Y-m-d', strtotime('-10 days'));
-    $orders = wc_get_orders(array(
-        'date_created' => '>' . $days_ago,
+    // get last 45 days order for popular products
+    $days_ago_45 = date('Y-m-d', $current_time - (45 * 24 * 3600));
+    $popular_orders = wc_get_orders(array(
+        'date_created' => '>' . $days_ago_45,
     ));
-    if ($orders) {
-        foreach ($orders as $order) {
-            foreach ($order->get_items() as $item_id => $item) { 
+    if ($popular_orders) {
+        foreach ($popular_orders as $order) {
+            foreach ($order->get_items() as $item) { 
                 $product_id = $item->get_product_id();
                 wp_set_object_terms($product_id, 'popular', 'product_cat',true); 
             }
         }
     }
-    set_transient('last_time', array('time' => 'value'), 24 * 3600);
-    // update_option('last_time', $current_time);
+    // get last 10 days order for trending products
+    $days_ago_10 = date('Y-m-d', $current_time - (10 * 24 * 3600));
+    $trending_orders = wc_get_orders(array(
+        'date_created' => '>' . $days_ago_10,
+    ));
+    if ($trending_orders) {
+        foreach ($trending_orders as $order) {
+            foreach ($order->get_items() as $item) { 
+                $product_id = $item->get_product_id();
+                wp_set_object_terms($product_id, 'trending', 'product_cat',true); 
+            }
+        }
+    }
+    update_option('last_time', $current_time);
 }
-
 ?>
